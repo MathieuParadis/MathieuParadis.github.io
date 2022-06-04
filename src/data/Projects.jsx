5e71d9196d12ef560185eb196d9d59be64babde3
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-06-04 08:44:29 +0800

    updated projects data

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -14,7 +14,7 @@ const projects = [
     img: helping_hand,
     url: "https://helpping-hand.herokuapp.com/",
     stack: "JavaScript, React, Sass, Ruby on Rails, PostgreSQL",
-    description: "Website still in development..."
+    description: "Helping Hand is an aid platform that connects people in need to willing volunteers in your community.."
   },
   {
     title: "Boulder Bike Tour",
