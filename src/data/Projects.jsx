8ef34b4119d2baabed6090a773a764386a900df8
MathieuParadis <mathieuyl.paradis@gmail.com>
2022-11-13 06:58:49 +0800

    updated projects

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -65,13 +65,6 @@ const projects = [
     stack: "JavaScript, Ruby, Ruby on Rails, PostgreSQL",
     description: "Feedtruck is an application that connects individuals with foodtrucks around their homes in Paris."
   },
-  {
-    title: "The Gossip Project", 
-    img: gossip_project_screenshot,
-    url: "https://gossip-project-mat-edition.herokuapp.com/",
-    stack: "Ruby, Ruby on Rails, PostgreSQL",
-    description: "The Gossip Project is a service displaying gossips posted by its users."
-  },
 ];
 
 export default projects;
