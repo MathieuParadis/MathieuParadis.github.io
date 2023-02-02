c9299acc7ade7c0e829cf4c70fda6fe8c1d86cfe
MathieuParadis <mathieuyl.paradis@gmail.com>
2023-02-03 02:13:01 +0800

    file designs.jsx

---
+++ src/data/Designs.jsx
@@ -0,0 +1,46 @@
+// ASSETS IMPORTS
+// LOGOS
+import aqueo from '../assets/images/designs/logos/aqueo.svg'
+import burger_queen from '../assets/images/designs/logos/burger_queen.png'
+import feedtruck from '../assets/images/designs/logos/feedtruck.png'
+import pappa_johnny from '../assets/images/designs/logos/pappa_johnny.png'
+import salad_bar from '../assets/images/designs/logos/salad_bar.png'
+import taco_truck from '../assets/images/designs/logos/taco_truck.png'
+import vegan_place from '../assets/images/designs/logos/vegan_place.png'
+import we_sushi from '../assets/images/designs/logos/we_sushi.png'
+
+// POSTERS
+import flower_market from '../assets/images/designs/posters/flower_market.png'
+
+// ILLUSTRATIONS
+import cat from '../assets/images/designs/posters/cat.png'
+import choco_tart from '../assets/images/designs/posters/choco_tart.png'
+import donut from '../assets/images/designs/posters/donut.png'
+import owl from '../assets/images/designs/posters/owl.png'
+import snail from '../assets/images/designs/posters/snail.png'
+
+const logos = [
+  { title: 'Aqueo', img: aqueo },
+  { title: 'Feedtruck', img: feedtruck },
+  { title: 'Burger Queen', img: burger_queen },
+  { title: 'Pappa Johnny', img: pappa_johnny },
+  { title: 'Salad bar', img: salad_bar },
+  { title: 'Taco Truck', img: taco_truck },
+  { title: 'Vegan place', img: vegan_place },
+  { title: 'We sushi', img: we_sushi }
+]
+
+const posters = [
+  { title: 'Flower Market', img: flower_market }
+]
+
+const illustrations = [
+  { title: 'Cat', img: cat },
+  { title: 'Choco', img: choco_tart },
+  { title: 'Blueberry donut', img: donut },
+  { title: 'Geometric owl', img: owl },
+  { title: 'Thirsty snail', img: snail },
+
+]
+
+export { logos, posters, illustrations}
