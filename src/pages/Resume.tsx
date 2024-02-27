9f8755b4b72456a03c726bc53bc6051ed5cab8ea
MathieuParadis <76945713+MathieuParadis@users.noreply.github.com>
2024-02-27 20:02:52 +0800

    Enhancement/styling/resume page (#33)
    * edited style
    
    * Edited classes
    
    * refactored classes

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -11,28 +11,26 @@ import WorkSection from '../components/resume/WorkSection'
 
 const Resume = (): JSX.Element => {
   return (
-    <div className='resume'>
-      <div className='page'>
-        <div className='content'>
-          <div className='head'>
-            <h1><strong>Mathieu Paradis</strong></h1>
-            <p className='mb-0'>French Web Developer based between Europe and Asia. Passionate about minimalism, sustainability, coffee and bread.</p>
-            <p className='mt-2'>I recently picked on design to develop beautiful and impactful interfaces.</p>
+    <div className='py-[30px] px-[40px] lg:py-[27px] lg:px-[50px] my-0 mx-[-45px] min-h-screen bg-[var(--basic-color-grey-light)]'>
+      <div className='p-[10px] md:p-[20px] lg:p-[30px] my-[-10px] mx-[-20px] md:mx-[45px] lg:mx-[100px] rounded bg-[var(--basic-color-white)] shadow-[0_4px_6px_-1px_var(--shadow-color1),0_2px_4px_-2px_var(--shadow-color2)]'>
+        <div className='mb-8'>
+          <h1 className='font-bold text-4xl md:text-5xl'>Mathieu Paradis</h1>
+          <p className='text-lg md:text-xl mt-3'>French Web Developer based between Europe and Asia. Passionate about minimalism, sustainability, coffee and bread.</p>
+          <p className='text-lg md:text-xl mt-2'>I recently picked on design to develop beautiful and impactful interfaces.</p>
+        </div>
+        <div className='flex flex-col lg:flex-row mt-12'>
+          <div className='w-full lg:w-1/2 pr-0 lg:pr-[10px]'>
+            <WorkSection />
+            <EducationSection />
           </div>
-          <div className='body d-flex flex-column flex-lg-row'>
-            <div className='first-section w-100 w-lg-50'>
-              <WorkSection />
-              <EducationSection />
-            </div>
-            <div className='second-section d-flex flex-column h-100 w-100 w-lg-50'>
-              <CodingLanguagesSection />
-              <DesignToolsSection />
-              <SpokenLanguagesSection />
-              <ContactSection />
-            </div>
+          <div className='flex flex-col h-full w-full lg:w-1/2 pl-0 lg:pl-[10px]'>
+            <CodingLanguagesSection />
+            <DesignToolsSection />
+            <SpokenLanguagesSection />
+            <ContactSection />
           </div>
         </div>
-      </div>
+        </div>
     </div>
   )
 }
