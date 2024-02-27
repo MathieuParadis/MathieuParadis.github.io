3450ae165867d2522a6910a8ee1ae8af36d8888f
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-02-27 19:51:07 +0800

    Edited classes

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -13,18 +13,18 @@ const Resume = (): JSX.Element => {
   return (
     <div className='py-[30px] px-[40px] lg:py-[27px] lg:px-[50px] my-0 mx-[-45px] min-h-screen bg-[var(--basic-color-grey-light)]'>
       <div className='page'>
-        <div className='content'>
-          <div className='head'>
-            <h1><strong>Mathieu Paradis</strong></h1>
-            <p className='mb-0'>French Web Developer based between Europe and Asia. Passionate about minimalism, sustainability, coffee and bread.</p>
-            <p className='mt-2'>I recently picked on design to develop beautiful and impactful interfaces.</p>
+        <div>
+          <div className='mb-8'>
+            <h1 className='font-bold text-4xl md:text-5xl'>Mathieu Paradis</h1>
+            <p className='text-lg md:text-xl mt-3'>French Web Developer based between Europe and Asia. Passionate about minimalism, sustainability, coffee and bread.</p>
+            <p className='text-lg md:text-xl mt-2'>I recently picked on design to develop beautiful and impactful interfaces.</p>
           </div>
-          <div className='body flex flex-col lg:flex-row'>
-            <div className='first-section w-100 w-lg-50'>
+          <div className='flex flex-col lg:flex-row mt-12'>
+            <div className='w-full lg:w-1/2 pr-0 lg:pr-[10px]'>
               <WorkSection />
               <EducationSection />
             </div>
-            <div className='second-section d-flex flex-column h-100 w-100 w-lg-50'>
+            <div className='flex flex-col h-full w-full lg:w-1/2 pl-0 lg:pl-[10px]'>
               <CodingLanguagesSection />
               <DesignToolsSection />
               <SpokenLanguagesSection />
