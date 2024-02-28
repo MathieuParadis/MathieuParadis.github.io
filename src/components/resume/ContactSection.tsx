b28fa794acc9ca2cde24d731e469daea49942367
MathieuParadis <76945713+MathieuParadis@users.noreply.github.com>
2024-02-28 11:28:49 +0800

    Enhancement/style/resume (#36)
    * edited style of resume components
    
    * improved styling

--- src/components/resume/ContactSection.tsx
+++ src/components/resume/ContactSection.tsx
@@ -16,24 +16,24 @@ const ContactSection = (): JSX.Element => {
   return (
     <div className='mb-[60px]'>
       <h3 className='text-2xl font-black mb-[10px]'>CONTACT</h3>
-      <div>
-        <div className='contact-box'>
+      <div className='flex flex-col gap-4'>
+        <div className='flex items- gap-2'>
           <img
             className='w-[30px] md:w-[35px]'
             src={darkMode ? mail_dark : mail}
             alt='Mail logo'
             loading="lazy"
           />
-          <p>mathieuyl.paradis@gmail.com</p>
+          <p className='m-0 text-lg'>mathieuyl.paradis@gmail.com</p>
         </div>
-        <div className='contact-box'>
+        <div className='flex items-center gap-2'>
         <img
             className='w-[30px] md:w-[35px]'
             src={darkMode ? linkedIn_logo_dark : linkedIn_logo}
             alt='Mail logo'
             loading="lazy"
           />
-          <p>linkedin.com/in/mathieu-p-26593498/</p>
+          <p className='m-0 text-lg'>linkedin.com/in/mathieu-p-26593498/</p>
         </div>
       </div>
     </div>
