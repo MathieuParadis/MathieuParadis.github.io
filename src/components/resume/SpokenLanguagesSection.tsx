26f4667a53bcf7a23c3d3de2f67585211cae12ff
MathieuParadis <76945713+MathieuParadis@users.noreply.github.com>
2024-02-28 12:06:28 +0800

    edited style (#38)

--- src/components/resume/SpokenLanguagesSection.tsx
+++ src/components/resume/SpokenLanguagesSection.tsx
@@ -10,37 +10,35 @@ import { spokenLanguages } from '../../data/resume'
 
 const CodingLanguageSection = (): JSX.Element => {
   return (
-    <div className='spoken-languages-section'>
+    <div className='mb-[60px]'>
       <h3 className='text-2xl font-black mb-[20px]'>LANGUAGES</h3>
-      <div className='content'>
-        {spokenLanguages.map((el): JSX.Element => {
-          return (
-            <div className='language-box' key={el.title}>
-              <p>{el.title}</p>
-              <div className='language-rating'>
-                {[...Array(el.actualRating)].map((el, i): JSX.Element => (
-                  <img
-                    key={i}
-                    className='language-circle'
-                    src={circle}
-                    alt='circle'
-                    loading="lazy"
-                  />
-                ))}
-                {[...Array(el.maxRating - el.actualRating)].map((el, i): JSX.Element => (
-                  <img
-                    key={i}
-                    className='language-circle'
-                    src={empty_circle}
-                    alt='circle'
-                    loading="lazy"
-                  />
-                ))}
-              </div>
+      {spokenLanguages.map((el): JSX.Element => {
+        return (
+          <div className='flex items-center w-full mb-4 gap-2' key={el.title}>
+            <p className='p-0 m-0 w-1/3 md:w-1/5 lg:w-1/3'>{el.title}</p>
+            <div className='flex justify-between w-[65%] md:w-[55%] lg:w-[65%]'>
+              {[...Array(el.actualRating)].map((el, i): JSX.Element => (
+                <img
+                  key={i}
+                  className='w-[30px]'
+                  src={circle}
+                  alt='circle'
+                  loading="lazy"
+                />
+              ))}
+              {[...Array(el.maxRating - el.actualRating)].map((el, i): JSX.Element => (
+                <img
+                  key={i}
+                  className='w-[30px]'
+                  src={empty_circle}
+                  alt='circle'
+                  loading="lazy"
+                />
+              ))}
             </div>
-          )
-        })}
-      </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
