84216fe1181d658b7f3762e34a20bada94a26fc2
MathieuParadis <76945713+MathieuParadis@users.noreply.github.com>
2024-03-04 15:01:59 +0800

    refactored routes (#46)

---
+++ src/data/routes.ts
@@ -0,0 +1,30 @@
+// TYPES IMPORTS
+import { type RouteType } from '../types/routeTypes'
+
+export const routes: RouteType[] = [
+  {
+    id: '0',
+    title: 'Home',
+    path: '/'
+  },
+  {
+    id: '1',
+    title: 'Projects',
+    path: '/projects'
+  },
+  {
+    id: '2',
+    title: 'Designs',
+    path: '/designs'
+  },
+  {
+    id: '3',
+    title: 'Resume',
+    path: '/resume'
+  },
+  {
+    id: '4',
+    title: 'Contact',
+    path: '/contact'
+  },
+]
