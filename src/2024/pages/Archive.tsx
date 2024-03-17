fb87f4e86a5397148e9f8103b51273a231025ad1
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-03-17 10:49:34 +0800

    added links in archive page

--- src/2024/pages/Archive.tsx
+++ src/2024/pages/Archive.tsx
@@ -1,14 +1,26 @@
 // REACT IMPORT
 import React from 'react'
 
+// REACT-ROUTER-DOM IMPORTS
+import { NavLink } from 'react-router-dom'
+
 const Archive = (): JSX.Element => {
   return (
     <div>
       <div>Archive page</div>
       <div className='flex'>
-        <div className=''>
-          
-        </div>
+        <NavLink
+          to={'/'}
+          end={true}
+        >
+          2024
+        </NavLink>
+        <NavLink
+          to={'/archive/2022'}
+          end={true}
+        >
+          2022
+        </NavLink>
       </div>
     </div>
   )
